0b617a1539175beddcfabe749b09e93708fefef6
Adejolaoluwa <84287904+JollyJola@users.noreply.github.com>
2023-04-18 07:32:16 +0100

    build

--- Predictive-Sales-Inventory-MS-master/src/api/api.js
+++ Predictive-Sales-Inventory-MS-master/src/api/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:4000",
-  // baseURL: "https://pnic-backend-production.up.railway.app",
+  // baseURL: "http://localhost:4000",
+  baseURL: "https://pnic-backend-production.up.railway.app",
   withCredentials: true,
   credentials: "include",
 });
